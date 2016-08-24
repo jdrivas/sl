1e4a086b11fa799e7b6762835b4e14105b809392
David Rivas <jdr@rivas-yee.com>
2016-08-24 15:04:27 -0700

    First commit.

---
+++ log.go
@@ -0,0 +1,68 @@
+package sl
+  
+import (
+  "github.com/Sirupsen/logrus"
+)
+
+type SL struct {
+  Logger *logrus.Logger
+}
+
+func New(l *logrus.Logger) (*SL) {
+  sl := new(SL)
+  sl.Logger = l
+  return sl
+}
+
+func (l *SL) CheckFatalError(fields logrus.Fields, msg string, err error) {
+  if err != nil { 
+    l.Fatal(fields, msg, err)
+  }
+}
+
+func (l *SL) Panic(fields logrus.Fields, msg string, err error) {
+  log := l.Logger
+  if err == nil {
+    log.WithFields(fields).Panic(msg)
+  } else {
+    log.WithFields(fields).WithError(err).Panic(msg)
+  }
+}
+
+func (l *SL) Fatal(fields logrus.Fields, msg string, err error) {
+  log := l.Logger
+  if err == nil {
+    log.WithFields(fields).Fatal(msg)
+  } else {
+    log.WithFields(fields).WithError(err).Fatal(msg)
+  }
+}
+
+func (l *SL) Error(fields logrus.Fields, msg string, err error) {
+  log := l.Logger
+  if err == nil {
+    log.WithFields(fields).Error(msg)
+  } else {
+    log.WithFields(fields).WithError(err).Error(msg)
+  }
+}
+
+func (l *SL) Warn(fields logrus.Fields, msg string) {
+  l.Logger.WithFields(fields).Warn(msg)
+}
+
+func (l *SL)Info(fields logrus.Fields, msg string) {
+  l.Logger.WithFields(fields).Info(msg)
+}
+
+func (l *SL) Debug(fields logrus.Fields, msg string) {
+  l.Logger.WithFields(fields).Debug(msg)
+}
+
+func (l *SL) SetFormatter(f logrus.Formatter) {
+  l.Logger.Formatter = f
+}
+
+func (l *SL) SetLevel(f logrus.Level) {
+  l.Logger.Level = f
+}
